eb0e04edc1bb5ce72a9d11c274f7cd275a9bd6a8
meaSec <muhammetemiraslan0@gmail.com>
2025-03-18 03:23:08 +0300

    login proccess

---
+++ account/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+# Create your views here.logout
+
+def login_request(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "account/login.html", {
+                "error": "username or password wrong!"
+            })
+
+    return render(request, "account/login.html")
+
+def register_request(request):
+    return render(request, "account/register.html")
+
+def logout_request(request):
+    return redirect("home")
